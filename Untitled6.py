00b0f575e2e82d0f44fa848bed3a20f145d999b5
saundrya <82432613+saundrya@users.noreply.github.com>
2021-05-09 21:59:42 -0700

    Add files via upload

---
+++ Untitled6.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[8]:
+
+
+s=input('enter word:')
+print(s[::-1])
+
+
+# In[74]:
+
+
+for i in range(5):
+    for j in range(0,i+1):
+        print("*",end=" ")
+    print("\n")
+for k in range(5):
+    for l in range(0,4-k):
+        print("*",end=" ")
+    print("\n")
+    
+        
+
+        
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
